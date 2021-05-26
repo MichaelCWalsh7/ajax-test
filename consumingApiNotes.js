7bb64b1bae33666a57957d31c7b7dfb2c3b4c297
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-05-26 13:24:29 +0000

    Started working with APIs

---
+++ consumingApiNotes.js
@@ -0,0 +1,54 @@
+function getData(cb) {
+    var xhr = new XMLHttpRequest();
+    // var data; - this was for prevous lines of code for example, the setTimeout.
+
+    xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+    xhr.send();
+
+
+
+
+
+    xhr.onreadystatechange = function () {
+        // console.log(this.readyState);
+        if (this.readyState == 4 && this.status == 200) {
+
+            cb(JSON.parse(this.responseText));
+
+            // There are many ready states to an XMLHttpRequest, raning from 0 which UNSENT, to 4 which is DONE
+            // First we check ready state of the request cleint.
+            // Next is the status code, here 200 = OK. Request succeeded
+
+            // document.getElementById("data").innerHTML = this.responseText;
+
+            // console.log(typeof(this.responseText));   This returns 'string' to the console as it is just a string in the form of json
+            // console.log(typeof(JSON.parse(this.responseText)));   This return 'object' to the console as now it is in json
+            // console.log(JSON.parse(this.responseText));   Returns the json content, i.e various objects/properties
+            // data = this.responseText;
+            // console.log(data);   Returns the object as the new 'data' variable, however we can only use this WITHIN this function. 
+
+            // setData(JSON.parse(this.responseText)); - This is for the deserialisation below. 
+        }
+    };
+}
+
+getData(function(data){
+    console.log(data);
+})
+
+function printDataToConsole(data) {
+    console.log(data);
+}
+
+getData(printDataToConsole);
+
+// function setData(jsonData) {
+//     data = jsonData;
+//     console.log(data);
+// } This code does something called 'deserialising' the json data
+
+// setTimeout(function () {
+//     console.log(data)
+// }, 500)
+/* The above function allows to take our data out of the function by executing after 500 miliseconds, giving the ready state plenty of time get to
+and avoiding logging 'undefined.' */
--- main.js
+++ main.js
@@ -1,32 +1,50 @@
-var xhr = new  XMLHttpRequest();
-var data;
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
-xhr.send();
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();
 
-// YOU NEED TO GO BACK AND REVIEW WTF IS HAPPENING WITH THIS VARIABLE IN THE PARENTHESIS!!!!!!
-function setData(jsonData) {
-    data = jsonData;
-    console.log(data);
+    xhr.open("GET", baseURL + type + '/');
+    xhr.send();
+
+    xhr.onreadystatechange = function () {
+        // console.log(this.readyState);
+        if (this.readyState == 4 && this.status == 200) {
+
+            cb(JSON.parse(this.responseText));
+        }
+    };
 }
 
+function getTableHeaders(obj) {
+    var tableHeaders = [];
 
-xhr.onreadystatechange = function() {
-    if ( this.readyState == 4 && this.status == 200) {
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`);
+    });
+    return `<td>${tableHeaders}</td>`;
+    
+}
 
-        // There are many ready states to an XMLHttpRequest, raning from 0 which UNSENT, to 4 which is DONE
-        // First we check ready state of the request cleint.
-        // Next is the status code, here 200 = OK. Request succeeded
+function writeToDocument(type) {
+    var el = document.getElementById("data");
+    el.innerHTML = "";
+    // The above two lines clear the html before printing the next one with a new button click. 
+    getData(type, function(data) {
+        data = data.results;
+        var tableHeaders = getTableHeaders(data[0]);
 
-        // document.getElementById("data").innerHTML = this.responseText;
+        // data.forEach(function (item) {
+        //     // Object.keys(item).forEach(function(key) {
+        //     //     console.log(key);
+        //     // })
+        //     // el.innerHTML += "<p>" + item.name; + "</p>"
+        // });
 
-        // console.log(typeof(this.responseText));   This returns 'string' to the console as it is just a string in the form of json
-        // console.log(typeof(JSON.parse(this.responseText)));   This return 'object' to the console as now it is in json
-        // console.log(JSON.parse(this.responseText));   Returns the json content, i.e various objects/properties
-        // data = this.responseText;
-        // console.log(data);   Returns the object as the new 'data' variable, however we can only use this WITHIN this function. 
+        el.innerHTML = `<table>${tableHeaders}</table>`;
 
-        setData(JSON.parse(this.responseText));
-    }
-};
+        /* Without the '.results' the html simply reads '[object Object]' with '.results', you get a list of 10 of these,
+        so it still needs to be unpacked further. */
 
+
+    });
+}
