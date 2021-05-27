d41201c9dcc73b5f5ab2fd954ef554dcd0e1155f
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-05-27 13:32:28 +0000

    Began to tabularise and truncate data from API

--- main.js
+++ main.js
@@ -26,12 +26,24 @@ function getTableHeaders(obj) {
 }
 
 function writeToDocument(type) {
+    var tableRows = [];
     var el = document.getElementById("data");
-    el.innerHTML = "";
+    
     // The above two lines clear the html before printing the next one with a new button click. 
     getData(type, function(data) {
         data = data.results;
         var tableHeaders = getTableHeaders(data[0]);
+
+        data.forEach(function(item) {
+            var dataRow = [];
+
+            Object.keys(item).forEach(function(key) {
+                var rowData = item[key].toString();
+                var truncatedData = rowData.substring(0, 15);
+                dataRow.push(`<td>${truncatedData}</td>`);
+            });
+            tableRows.push(`<tr>${dataRow}</tr>`);
+        })
 
         // data.forEach(function (item) {
         //     // Object.keys(item).forEach(function(key) {
@@ -40,7 +52,7 @@ function writeToDocument(type) {
         //     // el.innerHTML += "<p>" + item.name; + "</p>"
         // });
 
-        el.innerHTML = `<table>${tableHeaders}</table>`;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
 
         /* Without the '.results' the html simply reads '[object Object]' with '.results', you get a list of 10 of these,
         so it still needs to be unpacked further. */
